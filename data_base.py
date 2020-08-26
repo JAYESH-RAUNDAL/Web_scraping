883861ffa5fd5559eaeed0a8846e778353195064
JAYESH DILIP RAUNDAL <jdraundal@yahoo.com>
2020-08-26 21:32:35 +0530

    database connection uploaded

---
+++ data_base.py
@@ -0,0 +1,35 @@
+from mysql import connector
+
+conn = connector.connect(
+    host="localhost",
+    user="root",
+    passwd="9960551687",
+    database="injectsolar")
+
+curr = conn.cursor()
+
+def create_table():
+
+    curr.execute("drop table if exists error_log")
+    curr.execute("CREATE TABLE error_log ( \
+                            id INT AUTO_INCREMENT PRIMARY KEY, \
+                            device_name VARCHAR(100),  \
+                            invertor_name VARCHAR(100), \
+                            alarm VARCHAR(100), \
+                            occurance_time	 DATETIME, \
+                            clearance_time	 DATETIME, \
+                            message VARCHAR(100) \
+                            )")
+create_table()
+
+def table(list):
+    data = "INSERT INTO error_log ( device_name, invertor_name,alarm, occurance_time, clearance_time, message) VALUES (%s, %s, %s, %s, %s, %s)"
+    curr.executemany(data, list)
+    conn.commit()
+
+def fetch(item):
+    curr.execute(f"SELECT * FROM {item}")
+    curr.fetchall()
+
+    print('done')
+
