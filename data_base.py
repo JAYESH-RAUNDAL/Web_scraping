9ef11f0b53aac84785990b892cba450b2c6cf502
JAYESH DILIP RAUNDAL <jdraundal@yahoo.com>
2020-08-26 22:32:08 +0530

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -4,13 +4,13 @@ conn = connector.connect(
     host="localhost",
     user="root",
     passwd="9960551687",
-    database="injectsolar")
+    database="injectsolar")   #CONNECT TO MYSQL
 
 curr = conn.cursor()
 
 def create_table():
 
-    curr.execute("drop table if exists error_log")
+    curr.execute("drop table if exists error_log")  #DROPING TABLE TO AVOID ERROR
     curr.execute("CREATE TABLE error_log ( \
                             id INT AUTO_INCREMENT PRIMARY KEY, \
                             device_name VARCHAR(100),  \
@@ -20,11 +20,11 @@ def create_table():
                             clearance_time	 DATETIME, \
                             message VARCHAR(100) \
                             )")
-create_table()
+create_table()                                      #ERRO_LOG TABLE CREATED
 
 def table(list):
     data = "INSERT INTO error_log ( device_name, invertor_name,alarm, occurance_time, clearance_time, message) VALUES (%s, %s, %s, %s, %s, %s)"
-    curr.executemany(data, list)
+    curr.executemany(data, list)     #INSERT VALUES IN TABLE
     conn.commit()
 
 def fetch(item):
